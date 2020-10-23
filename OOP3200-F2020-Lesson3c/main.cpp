9a087dd6b52376c6e77f520f5889fca83e9c3825
Ahmed Butt <TypicalNewb@outlook.com>
2020-10-22 21:02:24 -0400

    basically done?

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -10,26 +10,25 @@
 #include <cstdlib>
 #include <vector>
 
-
 #include "Vector2D.h"
 
 int main()
 {
 	try
 	{
-		/************************
-		 *	DECLARATIONS
-		 ************************/
+		//DECLARATIONS
 		std::map<std::string, Vector2D<float>*> vector_map; //I'm the map
 		auto* def = new Vector2D<float>(); //default (0, 0) point
 		bool notFound = false;
 		std::ifstream infile;
 		std::string fileName;
 
+		//USER INPUT: File Name
 		std::cout << "Enter the name of the file you wish to open: ";
 		std::cin >> fileName;
 		infile.open(fileName.c_str());
-		
+
+		//VALIDATION: File Name
 		if (infile.fail()) //if file doesn't exist
 		{
 			std::cout << "\n" << fileName << " could not be opened for input. Check that the file exists.\n";
@@ -40,13 +39,13 @@ int main()
 		}
 		else //file found and opened
 		{
+			//DECLARATIONS: Total Distance and Number of Points
 			float x, y, totalDistance = 0;
 			std::string pointLabel;
 			int totalPoints = 0;
-			std::vector<std::string> pointLabels;
-			
-			//Check number of points and find total distance
-			while (!infile.fail())
+
+			//LOOP: Total Distance and Number of Points
+			while (!infile.fail()) //keep looping as long as automatic inputs don't fail
 			{
 				infile >> pointLabel;
 				infile.ignore(1, ' ');
@@ -65,39 +64,38 @@ int main()
 				//std::cout << pointLabel << " " << "(" << x << ", " << y << ")\n"; //show all points
 
 				totalPoints++; //increment to keep track of total points
-
-				pointLabels.resize(totalPoints);
-				pointLabels.push_back(pointLabel);
 				
 				auto* temp_vector = new Vector2D<float>(x, y);
-				vector_map[pointLabel] = temp_vector;
+				vector_map[pointLabel] = temp_vector; //add the temporary Vector2D to vector_map
 				
 				totalDistance += Vector2D<float>::Distance(*def,*temp_vector); //add distances
 
 				def = temp_vector; //change default Vector2D to old Vector2D so next increment finds distance between old and new
 			}
+			
 			std::cout << "\n" << "The map contains " << totalPoints << " points for a total distance of " << totalDistance << ".\n";
 			
-			//User input loop
+			//USER INPUT: Loop
 			std::string userInput;
-			*def = Vector2D<float>();
+			*def = Vector2D<float>(); //making def = default Vector2D (point (0, 0))
 			
-			while (userInput != "quit")
+			while (userInput != "quit") //loop as long as user doesn't type "quit"
 			{
 				std::cout << "\nEnter the label of the point you wish to go to (\"quit\" to end): ";
 				std::cin >> userInput;
 
-				for (const auto& current_vector : vector_map)
+				for (const auto& current_vector : vector_map) //loop for each data in vector_map
 				{
-					if (userInput == current_vector.first)
+					if (userInput == current_vector.first) //if a matching label is found
 					{
+						//Finding and displaying the distance between default Vector2D and user-selected Vector2D
 						const auto x2 = current_vector.second->GetX(), y2 = current_vector.second->GetY();
 						const auto distance = Vector2D<float>::Distance(*def,*current_vector.second);
 						std::cout << "The distance between AA (0, 0) and " << current_vector.first << " (" << x2 << ", " << y2 << ")" << " is " << distance << ".\n";
-						notFound = false;
+						notFound = false; 
 						break;
 					}
-					else
+					else //else make boolean notFound = true (to help display error message)
 					{
 						notFound = true;
 					}
@@ -111,7 +109,7 @@ int main()
 	}
 	catch(...)  // an exception was thrown
 	{
-		std::cout << "An error occurred at run-time: ";
+		std::cout << "\nAn error occurred at run-time.\n";
 	}
 
 	std::cout << "\n--------------------------------------------------------------------";
